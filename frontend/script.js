7b96547fdaec9ff13eb60152a3739e5022a62cde
Anja394e <93381068+Anja394e@users.noreply.github.com>
2024-09-26 20:10:26 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -40,6 +40,20 @@ function eingeladene_personen(ordentliche_mitglieder, ersatz_personen) {
             .sort((a, b) => a.listenplatz - b.listenplatz)[0]; // Person mit niedrigstem Listenplatz zuerst
     }
 
+    // Sucht die weibliche Ersatzperson mit dem niedrigsten Listenplatz
+function finde_niedrigste_weibliche_ersatzperson() {
+    return ersatz_personen
+        .filter(person => person.geschlecht === 'w' && !eingeladen.includes(person) && person.anwesend) // Nur weibliche, die noch nicht eingeladen sind
+        .sort((a, b) => a.listenplatz - b.listenplatz)[0]; // Niedrigster Listenplatz zuerst
+
+
+// Sucht die männliche Ersatzperson mit dem höchsten Listenplatz
+function finde_hoechste_maennliche_ersatzperson() {
+    return eingeladen
+        .filter(person => person.geschlecht === 'm' && person.liste === 2) // Nur Ersatzpersonen (Liste 2) und männlich
+        .sort((a, b) => b.listenplatz - a.listenplatz)[0]; // Höchster Listenplatz zuerst
+}
+    
     // Zunächst werden alle anwesenden ordentlichen Mitglieder eingeladen
     ordentliche_mitglieder.forEach(person => {
         if (person.anwesend) {

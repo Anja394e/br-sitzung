c9b37d213e6303c149cb503a9cb8a44739539377
Anja394e <93381068+Anja394e@users.noreply.github.com>
2024-09-27 09:44:28 +0200

    Create ersatzmanagement.js

---
+++ backend/ersatzmanagement.js
@@ -0,0 +1,15 @@
+function finde_hoechste_maennliche_ersatzperson() {
+    // Code zum Finden der höchsten männlichen Ersatzperson
+}
+
+function finde_niedrigste_weibliche_ersatzperson() {
+    // Code zum Finden der niedrigsten weiblichen Ersatzperson
+}
+
+function anzahl_weiblich() {
+    // Code zum Berechnen der Anzahl der weiblichen Personen
+}
+
+function eingeladene_personen(ordentliche_mitglieder, ersatz_personen) {
+    // Hauptlogik zur Einladung von Personen
+}

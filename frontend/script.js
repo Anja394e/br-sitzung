6d69a862f9051fca2ef872d73e615178a2f3b611
Anja394e <93381068+Anja394e@users.noreply.github.com>
2024-09-27 09:49:54 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -213,35 +213,7 @@ document.getElementById("personenForm").addEventListener("submit", function (e) 
     document.getElementById("personenForm").reset();
 });
 
-// Funktion zum Bearbeiten einer Person
-function bearbeitenPerson(index, liste) {
-    let person = liste === 1 ? ordentliche_mitglieder[index] : ersatz_personen[index];
 
-    document.getElementById("name").value = person.name;
-    document.getElementById("email").value = person.mail;
-    document.getElementById("geschlecht").value = person.geschlecht;
-    document.getElementById("liste").value = person.liste;
-    document.getElementById("anwesend").checked = person.anwesend;
-    document.getElementById("listenplatz").value = person.listenplatz; // Listenposition wird gefüllt
-
-    // Entferne die alte Person, damit die neue hinzugefügt werden kann
-    loeschenPerson(index, liste);
-}
-
-// Funktion zum Löschen einer Person
-function loeschenPerson(index, liste) {
-    if (liste === 1) {
-        ordentliche_mitglieder.splice(index, 1);
-    } else {
-        ersatz_personen.splice(index, 1);
-    }
-
-    // Personen in localStorage speichern
-    speicherePersonen();
-
-    // Aktualisiere die Tabelle
-    displayPersonen();
-}
 
 // Starte mit dem Anzeigen der Personen
 displayPersonen();

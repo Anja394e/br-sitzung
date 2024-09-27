f3ee3e20005e75c67e67dfff307440a825922806
Anja394e <93381068+Anja394e@users.noreply.github.com>
2024-09-27 09:44:43 +0200

    Create ui.js

---
+++ backend/ui.js
@@ -0,0 +1,11 @@
+function displayEingeladenePersonen(eingeladen, nachgeladen_fuer) {
+    // Code zum Anzeigen der eingeladenen Personen
+}
+
+document.getElementById("einladenButton").addEventListener("click", () => {
+    // Code für den Event-Listener des Einladen-Buttons
+});
+
+function displayPersonen() {
+    // Code zum Füllen der Tabellen mit ordentlichen Mitgliedern und Ersatzpersonen
+}

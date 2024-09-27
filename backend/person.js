3b1eaf36827702442cd9ac370ae927554d1c7679
Anja394e <93381068+Anja394e@users.noreply.github.com>
2024-09-27 09:44:04 +0200

    Create person.js

---
+++ backend/person.js
@@ -0,0 +1,22 @@
+class Person {
+    constructor(liste, listenplatz, geschlecht, name, mail, anwesend) {
+        this.liste = liste;
+        this.listenplatz = listenplatz;
+        this.geschlecht = geschlecht;
+        this.name = name;
+        this.mail = mail;
+        this.anwesend = anwesend;
+    }
+
+    toString() {
+        return `Person(Liste: ${this.liste}, Listenplatz: ${this.listenplatz}, Geschlecht: ${this.geschlecht}, Name: ${this.name}, E-Mail: ${this.mail}, Anwesend: ${this.anwesend})`;
+    }
+}
+
+function bearbeitenPerson(index, liste) {
+    // Code zum Bearbeiten der Person
+}
+
+function loeschenPerson(index, liste) {
+    // Code zum Löschen der Person
+}

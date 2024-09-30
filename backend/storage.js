609018482a7ca6e350174ab0ac2b23f96b5dd2b9
Anja394e <93381068+Anja394e@users.noreply.github.com>
2024-09-30 14:56:49 +0200

    Delete backend/storage.js

--- backend/storage.js
+++
@@ -1,4 +0,0 @@
-function speicherePersonen() {
-    localStorage.setItem("ordentliche_mitglieder", JSON.stringify(ordentliche_mitglieder));
-    localStorage.setItem("ersatz_personen", JSON.stringify(ersatz_personen));
-}
